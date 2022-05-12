0920dd527dea375188e2ed2926f11578d03bbd8b
kazunoki <kazukingh01@gmail.com>
2022-05-12 18:07:07 +0900

    modify

--- build/lib/kkmlmanager/optuna.py
+++ build/lib/kkmlmanager/optuna.py
@@ -9,6 +9,7 @@ logger = set_logger(__name__)
 __all__ = [
     "create_study",
     "load_study",
+    "model_parameter_search",
 ]
 
 
@@ -59,3 +60,60 @@ def load_study(storage: str, name: str):
     df = pd.concat([df, dfwk1, dfwk2, dfwk3], axis=1, ignore_index=False)
     logger.info("END")
     return df, study
+
+def model_parameter_search(
+    trial, params_search: dict=None,
+    string_model: str=None, string_fit: str=None, 
+    string_pred: str=None, string_eval: str=None,
+    params_const: dict=None,
+):
+    """
+    Reserved word is "_model", "_output"
+    Usage::
+        >>> import lightgbm as lgb
+        >>> import numpy as np
+        >>> train_x = np.random.rand(1000, 5)
+        >>> train_y = np.random.randint(0, 5, 1000)
+        >>> valid_x = np.random.rand(1000, 5)
+        >>> valid_y = np.random.randint(0, 5, 1000)
+        >>> from functools import partial
+        >>> func = partial(
+                model_parameter_search,
+                params_search={
+                    "min_child_weight" : "trial.suggest_loguniform('min_child_weight', 1e-8, 1e3)",
+                    "subsample"        : "trial.suggest_uniform('subsample', 0.5 , 0.99)",
+                    "colsample_bytree" : "trial.suggest_loguniform('colsample_bytree', 0.001, 1.0)",
+                    "reg_alpha"        : "trial.suggest_loguniform('reg_alpha',  1e-8, 1e3)",
+                    "reg_lambda"       : "trial.suggest_loguniform('reg_lambda', 1e-8, 1e3)",
+                },
+                string_model='''LGBMClassifier(
+                    min_child_weight=min_child_weight, subsample=subsample, 
+                    colsample_bytree=colsample_bytree, reg_alpha=reg_alpha, reg_lambda=reg_lambda                    
+                )''',
+                string_fit="_model.fit(train_x, train_y)",
+                string_pred="_model.predict_proba(valid_x)",
+                string_eval="-1 * np.log(_output[:, valid_y]).mean()",
+                params_const={
+                    "LGBMClassifier": lgb.LGBMClassifier, "np": np, 
+                    "train_x": train_x, "train_y": train_y, "valid_x": valid_x, "valid_y": valid_y
+                },
+            )
+        >>> create_study(func, 100, storage=None, is_new=True, name="test")
+    """
+    logger.info("START")
+    params = {}
+    for x, y in params_search.items():
+        params[x] = eval(y, {}, {"trial": trial})
+    params.update(params_const)
+    model = eval(string_model.strip(), {}, params)
+    logger.info(f"model: {model}")
+    logger.info("fitting...")
+    params.update({"_model": model})
+    eval(string_fit.strip(), {}, params)
+    logger.info("predict...")
+    output = eval(string_pred.strip(), {}, params)
+    params.update({"_output": output})
+    logger.info("evaluate...")
+    value  = eval(string_eval.strip(), {}, params)
+    logger.info("END")
+    return value
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='kkmlmanager',
-    version='1.0.3',
+    version='1.0.4',
     description='my object detection library.',
     long_description=long_description,
     long_description_content_type="text/markdown",
